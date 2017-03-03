661e7823677f83b968b7a9a6c3debb85e71bdaca
Phil Kunz <gitlab@philkunz.com>
2017-03-03 21:32:40 +0100

    update wrap

--- ts/smartipc.wrap.ts
+++ ts/smartipc.wrap.ts
@@ -2,7 +2,7 @@ import * as spawnWrap from 'spawn-wrap'
 
 let unwrap: any = null
 
-export let startSpawnWrap = (filePath: string, cliArgs: string[], envArgs: any) => {
+export let startSpawnWrap = (filePath: string, cliArgs: string[] = [], envArgs: any = {}) => {
   let spawnArray = [filePath]
   for (let cliArg of cliArgs) {
     spawnArray.push(cliArg)
